3b58fdbc08a27dbb1a55ade5fd81888a0816c73b
MarcoVCunha <126254095+MarcoVCunha@users.noreply.github.com>
2025-03-28 18:43:49 -0300

    second-commit

--- src/pages/CreatePost/CreatePost.js
+++ src/pages/CreatePost/CreatePost.js
@@ -1,8 +1,9 @@
 import styles from './CreatePost.module.css'
 
 import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { navigate, useNavigate } from 'react-router-dom'
 import { useAuthValue } from '../../context/AuthContext'
+import { useInsertDocument } from '../../hooks/useInsertDocument'
 
 
 const CreatePost = () => {
@@ -12,15 +13,53 @@ const CreatePost = () => {
   const [tags, setTags] = useState([])
   const [formError, setFormError] = useState("")
 
+  const {user} = useAuthValue()
+
+  const { insertDocument, response } = useInsertDocument("posts")
+
+  const navigate = useNavigate()
+
   const handleSubmite = (e) => {
-    e.prevenDefult();
+    e.preventDefault();
+    setFormError("")
+
+  // validade image URL
+
+  try {
+    new URL(image)
+  } catch (error) {
+    setFormError("A imagem precisa ser uma URL")
+  }
+
+  //criar o array de tags
+
+  const tagsArray = tags.split(",").map((tag) => tag.trim().toLowerCase())
+
+  //checar todos os valores 
+
+  if(!title || !image || !tags || !body){
+    setFormError("Por favor, preencha todos os campos!")
+  }
+
+  insertDocument({
+    title,
+    image,
+    body,
+    tagsArray,
+    uid: user.uid,
+    createBy: user.displayName,
+  })
+
+  // redirect to home page
+  navigate("/")
+
   }
 
   return (
     <div className={styles.create_post}>
       <h2>Criar post</h2>
       <p>Escreva sobre o que quiser e compartilhe o seu conhecimento!</p>
-      <form action={handleSubmite}>
+      <form onSubmit={handleSubmite}>
         <label>
           <span>Titulo:</span>
           <input 
@@ -40,7 +79,7 @@ const CreatePost = () => {
             required 
             placeholder='Insira uma imagem que represente seu post'
             onChange={(e) => setImage(e.target.value)}
-            value={Image}
+            value={image}
           />
         </label>
         <label>
@@ -64,14 +103,14 @@ const CreatePost = () => {
             value={tags}
           />
         </label>
-        <button className='btn'>Cadastrar</button>
-        {/*!loading && <button className='btn'>Cadastrar</button>}
-          {loading && (
+        {!response.loading && <button className='btn'>Cadastrar</button>}
+          {response.loading && (
             <button className='btn'>
             Aguarde...
           </button>
           )} 
-          {error && <p className='error'>{error}</p>*/}
+          {response.error && <p className='error'>{response.error}</p>}
+          {formError && <p className='error'>{formError}</p>}
       </form>
     </div>
   )
